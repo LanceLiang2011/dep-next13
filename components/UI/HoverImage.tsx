44e00995cf9d928afcc1794313798dd4ba13b1ec
Lance Liang <liangxianyi2011@gmail.com>
2023-02-21 01:19:22 -0500

    add hover img priority

--- components/UI/HoverImage.tsx
+++ components/UI/HoverImage.tsx
@@ -9,6 +9,7 @@ export default function HoverImage({ children, src, ...rest }: any) {
         className='absolute hidden top-0 left-1/2 transform -translate-x-1/2 -translate-y-full'
         src={src}
         alt='Hovered Image'
+        priority
         {...rest}
       />
     </span>
