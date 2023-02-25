386a2ef1827a9ce6e2ec0c86b5d7eae4ddeedb18
Lance Liang <liangxianyi2011@gmail.com>
2023-02-25 15:52:52 -0500

    make sidebar more refletive

--- app/knowledge/Sidebar.tsx
+++ app/knowledge/Sidebar.tsx
@@ -9,41 +9,39 @@ export default function Sidebar() {
     'hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white';
   const activeSegment = useSelectedLayoutSegment();
   return (
-    <>
-      <div className='w-48 h-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white mr-6 mt-2'>
-        <Link
-          href='/knowledge'
-          className={`block w-full px-4 py-2   border-b border-gray-200 rounded-t-lg cursor-pointer dark:border-gray-600 ${
-            !activeSegment ? selectedColor : unselectedColor
-          }`}
-        >
-          Home
-        </Link>
-        <Link
-          href='/knowledge/de'
-          className={`block w-full px-4 py-2 border-b border-gray-200 cursor-pointer  dark:border-gray-600 ${
-            activeSegment === 'de' ? selectedColor : unselectedColor
-          }`}
-        >
-          Disordered Eating
-        </Link>
-        <Link
-          href='/knowledge/body'
-          className={`block w-full px-4 py-2 border-b border-gray-200 cursor-pointer  dark:border-gray-600 ${
-            activeSegment === 'body' ? selectedColor : unselectedColor
-          }`}
-        >
-          Body
-        </Link>
-        <Link
-          href='/knowledge/mind'
-          className={`block w-full px-4 py-2 border-b border-gray-200 cursor-pointer dark:border-gray-600 ${
-            activeSegment === 'mind' ? selectedColor : unselectedColor
-          }`}
-        >
-          Mind
-        </Link>
-      </div>
-    </>
+    <div className='w-48 h-fit text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white mr-6 mt-2'>
+      <Link
+        href='/knowledge'
+        className={`block w-full px-4 py-2   border-b border-gray-200 rounded-t-lg cursor-pointer dark:border-gray-600 ${
+          !activeSegment ? selectedColor : unselectedColor
+        }`}
+      >
+        Home
+      </Link>
+      <Link
+        href='/knowledge/de'
+        className={`block w-full px-4 py-2 border-b border-gray-200 cursor-pointer  dark:border-gray-600 ${
+          activeSegment === 'de' ? selectedColor : unselectedColor
+        }`}
+      >
+        Disordered Eating
+      </Link>
+      <Link
+        href='/knowledge/body'
+        className={`block w-full px-4 py-2 border-b border-gray-200 cursor-pointer  dark:border-gray-600 ${
+          activeSegment === 'body' ? selectedColor : unselectedColor
+        }`}
+      >
+        Body
+      </Link>
+      <Link
+        href='/knowledge/mind'
+        className={`block w-full px-4 py-2 border-b border-gray-200 cursor-pointer dark:border-gray-600 ${
+          activeSegment === 'mind' ? selectedColor : unselectedColor
+        }`}
+      >
+        Mind
+      </Link>
+    </div>
   );
 }
--- app/knowledge/layout.tsx
+++ app/knowledge/layout.tsx
@@ -6,7 +6,7 @@ export default function KnowledgeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className=' flex gap-10 bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
+    <div className='flex flex-col md:flex-row md:gap-10 bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
       <Sidebar />
       <div className='flex justify-center'>{children}</div>
     </div>
