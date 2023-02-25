af67b393d691e469665786ccff3e6f3772260dd6
Lance Liang <liangxianyi2011@gmail.com>
2023-02-25 07:35:11 -0500

    fix bugs

--- app/journal/diary/[id]/page.tsx
+++ app/journal/diary/[id]/page.tsx
@@ -43,15 +43,16 @@ export default function Diary({ params }: any) {
           <Badge type='default'>{diary.date}</Badge>
           <br />
           <Paragraph>{diary.description}</Paragraph>
+          <br />
+          <Link href='/journal/diary'>
+            <button
+              type='button'
+              className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+            >
+              Back
+            </button>
+          </Link>
         </article>
-        <Link href='/journal/diary'>
-          <button
-            type='button'
-            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
-          >
-            Back
-          </button>
-        </Link>
       </div>
     </main>
   );
