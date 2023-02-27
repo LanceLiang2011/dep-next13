617c92fc44c1a4241030a946a5eb21c4eb95d583
Lance Liang <liangxianyi2011@gmail.com>
2023-02-26 21:22:33 -0500

    Add loading spin to goals list

--- app/journal/goals/GoalList.tsx
+++ app/journal/goals/GoalList.tsx
@@ -1,23 +1,29 @@
 'use client';
 import { auth, getUserGoalsById } from '@/library/firebase';
-import { useEffect } from 'react';
+import { Spinner } from 'flowbite-react';
+import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import GoalCard from './GoalCard';
 
 export default function GoalList({ setGoals, goals }: any) {
   const [user, loading, error] = useAuthState(auth);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     const getUserGoals = async () => {
+      setIsLoading(true);
       const goalsToSet = await getUserGoalsById(user?.uid ?? '');
       setGoals(goalsToSet);
+      setIsLoading(false);
     };
     getUserGoals();
   }, [user, setGoals]);
   return (
     <div className='flex flex-col gap-6 max-w-xl break-words'>
-      {goals?.map((goal: any) => (
-        <GoalCard key={goal.goalId} goal={goal} setGoals={setGoals} />
-      ))}
+      {!isLoading &&
+        goals?.map((goal: any) => (
+          <GoalCard key={goal.goalId} goal={goal} setGoals={setGoals} />
+        ))}
+      {isLoading && <Spinner aria-label='Waiting for data' size='xl' />}
     </div>
   );
 }
