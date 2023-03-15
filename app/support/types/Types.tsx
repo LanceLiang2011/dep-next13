e3f1cd71945339614d8f6dd4d7da7ef9658f11df
Lance Liang <liangxianyi2011@gmail.com>
2023-03-15 17:33:29 -0400

    Fix typo

--- app/support/types/Types.tsx
+++ app/support/types/Types.tsx
@@ -16,7 +16,7 @@ export default function Types() {
     <main className='pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900'>
       <div className='flex justify-between px-4 mx-auto max-w-screen-xl '>
         <article className='w-full format format-sm sm:format-base lg:format-lg format-blue dark:format-invert'>
-          <Title type='h1'>Types of Support</Title>
+          <Title type='h1'>Types of Support That Exist</Title>
           <Paragraph>
             When looking for support, it is important to know what types of
             support are available so you can find the support most beneficial to
@@ -37,7 +37,7 @@ export default function Types() {
                     What their Profession Does/Accreditation
                   </th>
                   <th scope='col' className='px-6 py-3'>
-                    What they can treat
+                    What they can help with
                   </th>
                 </tr>
               </thead>
