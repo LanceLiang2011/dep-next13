dfa65bfb1b3604d9eb526b07d84796180030b77e
Lance Liang <liangxianyi2011@gmail.com>
2023-03-22 16:39:37 -0400

    Fix typo

--- app/knowledge/HomePage.tsx
+++ app/knowledge/HomePage.tsx
@@ -13,20 +13,19 @@ export default function HomePage() {
       <div className='py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6'>
         <div className='max-w-screen-md mb-8 lg:mb-16'>
           <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>
-            Knowledge: What you need to know about Disordered Eating
+            What you need to know about Disordered Eating
           </h2>
           <p className='text-gray-500 sm:text-xl dark:text-gray-400'>
             Disordered eating is a term used to describe a range of abnormal
             eating behaviours that do not warrant the diagnosis of an eating
-            disorder. To learn more about it, click sections below that
-            interests you or use the navigation bar on the left side to further
-            explore.
+            disorder. To learn more about disordered eating, click on the
+            sections below or use the navigation bar on the left.
           </p>
         </div>
         <div className='space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0'>
           <Link href='/knowledge/de/whatis'>
             <Feature
-              title='What is Disordered Eating'
+              title='What is Disordered Eating?'
               content="Let's explore the definition of disordered eating and the
               continuum of eating together!"
               icon={<BsBookFill size={20} color='#2563eb' />}
@@ -35,16 +34,15 @@ export default function HomePage() {
 
           <Link href='/knowledge/de/whyandhow'>
             <Feature
-              title='Why and How People Engage in Disordered Eating'
-              content="Why and how people engage in disordered eating? Let's look
-            into it in detail to figure out."
+              title='Why and How Do People Engage in Disordered Eating?'
+              content="Why and how do people engage in disordered eating? Let's deep dive into this further!"
               icon={<BsFillPatchQuestionFill size={20} color='#2563eb' />}
             />
           </Link>
 
           <Link href='/knowledge/body/body'>
             <Feature
-              title='Body composition'
+              title='Body Composition'
               content="How is our Body Composed? Let's explore!"
               icon={<IoIosBody size={20} color='#2563eb' />}
             />
@@ -53,7 +51,7 @@ export default function HomePage() {
           <Link href='/knowledge/body/exercise'>
             <Feature
               title='Physical Activity & Exercise'
-              content='There are so many exercises out there! What are they and which is
+              content='There are so many different types of exercise out there! What are they and which is
             best for me?'
               icon={<MdSportsHandball size={20} color='#2563eb' />}
             />
@@ -69,8 +67,8 @@ export default function HomePage() {
 
           <Link href='/knowledge/mind/influence'>
             <Feature
-              title='How Does Mind Influence Eating?'
-              content='Understanding how we interpret situations, how we individually think, how we show emotions, and how these components can influence us.'
+              title='How Does Our Mind Influence Our Eating?'
+              content='Understand how our emotions, thinking patterns, and life situations all influence the way we eat.'
               icon={<FaBrain size={20} color='#2563eb' />}
             />
           </Link>
