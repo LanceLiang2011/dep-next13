03db7a01ff0974da663f486e06c46330482dabc4
Lance Liang <liangxianyi2011@gmail.com>
2023-04-03 09:29:19 -0400

    update color

--- app/journal/layout.tsx
+++ app/journal/layout.tsx
@@ -14,7 +14,7 @@ export default function JournalLayout({
   if (!user)
     return (
       <div className='flex flex-col items-center max-w-2xl mx-auto my-36 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'>
-        <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+        <h5 className='mb-2 text-2xl font-bold tracking-tight text-heading dark:text-white'>
           You need to Log in to use Journal
         </h5>
         <p className='font-normal text-gray-700 dark:text-gray-400'>
@@ -23,7 +23,7 @@ export default function JournalLayout({
         <Link href='/login'>
           <button
             type='button'
-            className='mt-8 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+            className='mt-8 text-heading bg-button-back hover:bg-bg-hover focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
           >
             Login Page
           </button>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/navigation';
 
 // ? stateless import
-import logo from '@/resources/imgs/trp.webp';
+import logo from '../../resources/imgs/my_eating_journey_symbol_logo.webp';
 import Image from 'next/image';
 import Link from 'next/link';
 import { BsGoogle } from 'react-icons/bs';
@@ -49,14 +49,14 @@ export default function Login() {
         </Link>
         <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
           <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
-            <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
+            <h1 className='text-xl font-bold leading-tight tracking-tight text-heading md:text-2xl dark:text-white'>
               Login with Google or your account
             </h1>
             <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
               <div>
                 <label
                   htmlFor='email'
-                  className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+                  className='block mb-2 text-sm font-medium text-body dark:text-white'
                 >
                   Your email
                 </label>
@@ -66,7 +66,7 @@ export default function Login() {
                   id='email'
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className='bg-gray-50 border border-gray-300 text-body sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                   placeholder='name@company.com'
                   required
                 />
@@ -91,7 +91,7 @@ export default function Login() {
               </div>
               <button
                 type='submit'
-                className='w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+                className='w-full text-white bg-subheading hover:bg-heading focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
               >
                 Log In
               </button>
@@ -101,7 +101,7 @@ export default function Login() {
                   await signInWithGoogle();
                   router.push('/journal');
                 }}
-                className='w-full text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
+                className='w-full text-white bg-button-hover hover:bg-button-text focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800'
               >
                 <span className='flex flex-row items-center gap-3 justify-center'>
                   <BsGoogle />
