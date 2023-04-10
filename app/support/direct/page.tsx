5caa25348b800c6a722eafcd01d1f0f4f9bb5377
Lance Liang <liangxianyi2011@gmail.com>
2023-04-10 08:36:48 -0400

    Add nedic helpline

--- app/support/direct/page.tsx
+++ app/support/direct/page.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import Title from '@/components/Typograpjy/Title';
+import Paragraph from '@/components/Typograpjy/Paragraph';
 
 export default function DirectPage() {
-  return <div>Under Construction. Plese come back later.</div>;
+  return (
+    <div>
+      <Title type='h1'>
+        National Eating Disorder Information Centre (NEDIC)
+      </Title>
+      <br />
+      <Paragraph>
+        NEDIC provides information, resources, referrals and support to
+        Canadians affected by eating disorders. You may like to visit{' '}
+        <a
+          className=' text-blue-500'
+          href='https://nedic.ca/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          their website
+        </a>{' '}
+        for further information
+      </Paragraph>
+      <br />
+      <a
+        className='button-blue inline-block'
+        href='https://nedic.ca/blog/what-can-the-helpline-do-for-me/'
+        target='_blank'
+        rel='noreferrer'
+      >
+        What Can The Helpline Do For Me?
+      </a>
+    </div>
+  );
 }
