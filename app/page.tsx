e3c2c3ce18f91b6022c85a1f2f571fdc47ac7f27
Lance Liang <liangxianyi2011@gmail.com>
2023-04-21 13:44:03 -0400

    Add module

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,60 @@
+"use client";
+import { Button, Modal } from "flowbite-react";
 import Image from "next/image";
+import React, { useState } from "react";
 import homeImage from "../resources/imgs/my_eating_journey_home_page.png";
-import Paragraph from "@/components/Typograpjy/Paragraph";
 
 export default function Home() {
+  const [isModalVisible, setIsModalVisible] = useState(true);
+  const onClick = () => {
+    setIsModalVisible(!isModalVisible);
+  };
+
+  const onClose = () => {
+    setIsModalVisible(false);
+  };
   return (
-    <div>
+    <React.Fragment>
       <div className=" relative">
         <Image
           className="object-cover w-11/12 h-11/12"
           alt="home image"
           src={homeImage}
         />
-        {/* <p className="tagline absolute bottom-0 w-full text-center bg-opacity-30 bg-button-back text-2xl font-bold text-[#85696e] p-2">
-          empowering your journey to healthier eating habits and positive body
-          image
-        </p> */}
       </div>
-      {/* <br />
-      <Paragraph className="">
-        At &quot;My Eating Journey,&quot; we are dedicated to empowering
-        individuals throughout their journey to a healthier relationship with
-        food and positive body image. Our app provides personalized journaling,
-        education, and support to help users gain a deeper understanding of
-        their minds and bodies. It is our mission to empower our users
-        throughout their eating journey to achieve a more fulfilling life
-        through an understanding of nutrition, exercise, mindfulness, and
-        self-care.
-      </Paragraph>
-      <Paragraph className="">
-        A personalized web app designed to support individuals in developing a
-        positive and healthy relationship with food and body image. Offering a
-        range of educational content, personalized journaling programs, and
-        support resources to guide and empower users on their journey towards
-        healthier eating habits and overall well-being.
-      </Paragraph> */}
-    </div>
+      <Modal show={isModalVisible} onClose={onClose}>
+        <Modal.Header>
+          empowering your journey to healthier eating habits and positive body
+        </Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              At &quot;My Eating Journey,&quot; we are dedicated to empowering
+              individuals throughout their journey to a healthier relationship
+              with food and positive body image. Our app provides personalized
+              journaling, education, and support to help users gain a deeper
+              understanding of their minds and bodies. It is our mission to
+              empower our users throughout their eating journey to achieve a
+              more fulfilling life through an understanding of nutrition,
+              exercise, mindfulness, and self-care.
+            </p>
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              A personalized web app designed to support individuals in
+              developing a positive and healthy relationship with food and body
+              image. Offering a range of educational content, personalized
+              journaling programs, and support resources to guide and empower
+              users on their journey towards healthier eating habits and overall
+              well-being.
+            </p>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onClick}>I Understood!</Button>
+          {/* <Button color="gray" onClick={onClick}>
+            Decline
+          </Button> */}
+        </Modal.Footer>
+      </Modal>
+    </React.Fragment>
   );
 }
