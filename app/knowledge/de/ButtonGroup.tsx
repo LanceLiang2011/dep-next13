7aebce3adc1eaac7a1ec162e260f564a6fd2bece
Lance Liang <liangxianyi2011@gmail.com>
2023-04-27 14:46:39 -0400

    change knowledge page nav

--- app/knowledge/de/ButtonGroup.tsx
+++ app/knowledge/de/ButtonGroup.tsx
@@ -1,49 +1,62 @@
-'use client';
-import Link from 'next/link';
-import { useSelectedLayoutSegment } from 'next/navigation';
+"use client";
+// import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useSelectedLayoutSegment } from "next/navigation";
 
 export default function ButtonGroup() {
   const activeSegment = useSelectedLayoutSegment();
+  const router = useRouter();
   return (
-    <div className='mb-6 flex gap-6'>
-      <Link href='/knowledge/de/whatis'>
-        <button
-          className={`button-blue-sm ${
-            !activeSegment || activeSegment === 'whatis'
-              ? 'bg-bg-hover'
-              : undefined
-          }`}
-        >
-          Understanding Disordered Eating{' '}
-        </button>
-      </Link>
-      <Link href='/knowledge/de/whyandhow'>
-        <button
-          className={`button-blue-sm ${
-            activeSegment === 'whyandhow' ? 'bg-bg-hover' : undefined
-          }`}
-        >
-          Causes & Mechanisms of Disordered Eating
-        </button>
-      </Link>
-      <Link href='/knowledge/de/when'>
-        <button
-          className={`button-blue-sm ${
-            activeSegment === 'when' ? 'bg-bg-hover' : undefined
-          }`}
-        >
-          Identifying Problematic Disordered Eating
-        </button>
-      </Link>
-      <Link href='/knowledge/de/importance'>
-        <button
-          className={`button-blue-sm ${
-            activeSegment === 'importance' ? 'bg-bg-hover' : undefined
-          }`}
-        >
-          Significance of Understanding Disordered Eating
-        </button>
-      </Link>
+    <div className="mb-6 flex gap-6 items-stretch">
+      {/* <Link href="/knowledge/de/whatis"> */}
+      <button
+        onClick={() => {
+          router.push("/knowledge/de/whatis");
+        }}
+        className={`button-blue-sm ${
+          !activeSegment || activeSegment === "whatis"
+            ? "bg-bg-hover"
+            : undefined
+        }`}
+      >
+        Understanding Disordered Eating{" "}
+      </button>
+      {/* </Link> */}
+      {/* <Link href="/knowledge/de/whyandhow"> */}
+      <button
+        onClick={() => {
+          router.push("/knowledge/de/whyandhow");
+        }}
+        className={`button-blue-sm ${
+          activeSegment === "whyandhow" ? "bg-bg-hover" : undefined
+        }`}
+      >
+        Causes & Mechanisms of Disordered Eating
+      </button>
+      {/* <Link href="/knowledge/de/when"> */}
+      <button
+        onClick={() => {
+          router.push("/knowledge/de/when");
+        }}
+        className={`button-blue-sm ${
+          activeSegment === "when" ? "bg-bg-hover" : undefined
+        }`}
+      >
+        Identifying Problematic Disordered Eating
+      </button>
+      {/* </Link> */}
+      {/* <Link href="/knowledge/de/importance"> */}
+      <button
+        onClick={() => {
+          router.push("/knowledge/de/importance");
+        }}
+        className={`button-blue-sm ${
+          activeSegment === "importance" ? "bg-bg-hover" : undefined
+        }`}
+      >
+        Significance of Understanding Disordered Eating
+      </button>
+      {/* </Link> */}
     </div>
   );
 }
